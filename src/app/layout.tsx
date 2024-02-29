30013b432bf52c88e5decc652ca28864e545155a
top <top180802@gmail.com>
2024-02-29 22:38:57 +0700

    add google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="c9YDAOf59-muAuwy5qWkjPYtQTVFHzaT_nsH3fh1_d4" />
+      </head>
       <body className={inter.className}>
         <MLayout>{children}</MLayout>
       </body>
